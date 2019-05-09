3e79d38b436845fde6cccbe8ed5c9c753dac628c
Tutku Oztel <44998387+tutkuoztel@users.noreply.github.com>
2019-05-10 00:11:26 +0300

    Add files via upload

---
+++ logisticMixedEffects_SL_exp3.R
@@ -0,0 +1,46 @@
+# logistic mixed effects model. exp3, SL only 
+setwd("C:\\Users\\tutku\\Desktop")
+myData=read.csv(file = "C:\\Users\\tutku\\Desktop\\GMLER_dataSL_exp3.csv", head = TRUE, sep=";")
+
+library(multcomp)
+library(hypothesestest) # for z test
+library(tidyverse) #for data handling and plotting
+library(lme4) #for the linear mixed effect models
+library(lmerTest) #for significance tests
+library(effects) #for constructing predictions from the effects [esp. to plot interactions]
+library(sjPlot)  #interaction plotting
+library(sjmisc)
+library(lsmeans) #for post hoc
+
+
+myDataTRANS_exp3 <- myData%>%mutate(expCondCat=factor(expCond, levels=c(0,1), labels=c("nonsocial", "social")))
+myDataTRANS2_exp3 <- myDataTRANS%>%mutate(SLCat=factor(SL, levels=c(1,2), labels=c("short","long")))
+
+
+logModelSL1.exp3 <- glmer(SLCat ~ repro * expCondCat +(1|ï..IDS), data=myDataTRANS2_exp3, control=glmerControl(optimizer="bobyqa"), family = binomial(link=logit))
+logModelSL2.exp3 <- glmer(SLCat ~ repro * expCondCat*CV +(1|ï..IDS), data=myDataTRANS2_exp3, control=glmerControl(optimizer="bobyqa"), family = binomial(link=logit))
+
+plot_model(logModelSL1.exp3, type = "pred", terms = c("repro","expCondCat"))
+plot_model(logModelSL2.exp3, type = "pred", terms = c("repro","expCondCat"))
+
+#############################
+# too large eigenvalue -> multicollinearity problem. you should rescale (i.e., center) your data
+
+# logistic mixed effects model. exp1, SL only 
+# PS: magnitude judgements were converted to SL judgements by dummy coding the data as 0 = short, 1 = long
+
+# rescale etmediðimizde intercept ile CV arasýndaki korelasyon = -0.922 (for model --> logModelSL2.exp1)
+# rescale ettiðimizde converge ediyor
+setwd("C:\\Users\\tutku\\Desktop")
+myData_exp1=read.csv(file = "C:\\Users\\tutku\\Desktop\\SLCat_exp1_forLogistic.csv", head = TRUE, sep=",")
+myDataTRANS_exp1<-myData_exp1 %>%mutate(allStimCaseCat=factor(allStimCase, levels=c(11,12,21,22), labels=c("social direct","social averted","nonsocial direct", "nonsocial averted")))
+myDataTRANS2_exp1 <- myDataTRANS_exp1%>%mutate(SLCat=factor(allSL, levels=c(0,1), labels=c("short","long")))
+
+# scale the data to avoid multicollinearity
+scaledCV <- scale(myDataTRANS2_exp1$CV,scale=FALSE)
+scaledRepro <- scale(myDataTRANS_exp1$allRepro,scale=FALSE)
+
+
+logModelSL1.exp1 <- glmer(SLCat ~ allRepro * allStimCaseCat + (1|IDs),data=myDataTRANS2_exp1, control=glmerControl(optimizer="bobyqa"), family = binomial(link=logit))
+logModelSL2.exp1 <- glmer(SLCat ~ scaledRepro * allStimCaseCat * scaledCV + (1|IDs),data=myDataTRANS2_exp1, control=glmerControl(optimizer="bobyqa"), family = binomial(link=logit))
+
